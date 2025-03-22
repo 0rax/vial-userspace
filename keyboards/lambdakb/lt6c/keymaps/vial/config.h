602fd7cc462121bda34fef656b91d1b5753ba3ae
JP Roemer <2822534+0rax@users.noreply.github.com>
2025-03-22 02:41:42 +0100

    lt6c: add vial keymap overrides (DragScroll config)

---
+++ keyboards/lambdakb/lt6c/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFA, 0x99, 0xF4, 0x92, 0x56, 0x9E, 0x36, 0xD1}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 3, 5 }
+
+#define PLOOPY_DRAGSCROLL_MOMENTARY
+#define PLOOPY_DRAGSCROLL_INVERT
